0b80c52d747baed5a7d8b53fec1ba9899177b06d
August463 <mafia463@mail.ru>
2020-09-04 22:27:29 +0700

    Задание 1

--- src/ru/geekbrains/lesson_a/home/Exercise_1.java
+++ src/ru/geekbrains/lesson_a/home/Exercise_1.java
@@ -1,7 +1,18 @@
 package ru.geekbrains.lesson_a.home;
 
-
-
 public class Exercise_1 {
-    h
+    public static double sum(int a, int b, int c, int d){
+        System.out.println();
+        int result = a * (b + (c / d));
+        return result;
+    }
+    public static void main(String[] args) {
+
+        int x = 8;
+        int y = 10;
+        int z = 12;
+        int w = 18;
+        System.out.print(sum(x, y, z, w));
+
+    }
 }
