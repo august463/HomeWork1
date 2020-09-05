e132c911ce2d1b433101d7d0c1cc990ddd86dd77
August463 <mafia463@mail.ru>
2020-09-05 17:48:05 +0700

    Задание 4

---
+++ src/ru/geekbrains/lesson_a/home/Exercise_4.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.lesson_a.home;
+
+public class Exercise_4 {
+    public static void main(String[] args) {
+        userName("Александр");
+    }
+    public static void userName(String name) {
+        System.out.println("Привет, " + name + "!");
+    }
+}
