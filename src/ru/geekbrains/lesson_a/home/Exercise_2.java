e2decd6a1392e78b55d97d6bfe5ca71d3e8c6365
August463 <mafia463@mail.ru>
2020-09-05 17:25:11 +0700

    Задание 3

--- src/ru/geekbrains/lesson_a/home/Exercise_2.java
+++ src/ru/geekbrains/lesson_a/home/Exercise_2.java
@@ -1,4 +1,16 @@
 package ru.geekbrains.lesson_a.home;
 
 public class Exercise_2 {
+    public static void main(String[] args) {
+        System.out.println(task10and20 (5, 8));
+    }
+    public static boolean task10and20(int x, int y) {
+        System.out.println();
+        int sum = x + y;
+        if (sum > 10 && sum < 20) return true;
+        else return false;
+    }
+
+
 }
+
